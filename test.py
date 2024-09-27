b2680cfee2646801f8fea1be7268208293bd32a8
Peyton <peyton3995@outlook.com>
2024-09-27 10:51:20 -0400

    Adds needed API calls and multiple pages

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
 from flask import Flask
+import requests
+import json
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>test</p>"
+response = requests.get("https://api.weatherusa.net/feed?type=forecast&units=e&daily=0&maxtime=0h&q=39.726,-85.891&tz=US/Eastern")
+response.raise_for_status()
+
+jsonResponse = response.json()
+
+@app.route("/temperature")
+def temperature():
+
+    return f'<p>In New Palestine, Indiana: the temperature is {jsonResponse[0]["temp"]} faherenheit</p>'
+
+@app.route("/wind")
+def wind():
+
+    return f'<p>In New Palestine, Indiana: the wind speed is {jsonResponse[0]["wspd"]} miles per hour</p>'
+
+@app.route("/weather")
+def weather():
+
+    return f'<p>In New Palestine, Indiana: the current weather is: {jsonResponse[0]["wx_str"]}</p>'
+
