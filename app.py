d0aa82d5bd26b6f7a6beae3063d5078ff233edbf
Peyton <peyton3995@outlook.com>
2024-10-02 18:44:09 -0400

    Removes pycache from remote

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import requests
 
 app = Flask(__name__)
 
+# Function for making API calls
+# Makes request to given URL, fails if not 200
+
 def get_weather():
     response = requests.get("https://api.weatherusa.net/feed?type=forecast&units=e&daily=0&maxtime=0h&q=39.726,-85.891&tz=US/Eastern")
 
@@ -11,8 +14,10 @@ def get_weather():
     else:
         return None
 
+# Variable for storing results from API call
 weather_data = get_weather()
 
+# Each template receieves a given piece of data from the API call
 @app.route("/")
 def index():
     return render_template('index.html',day=weather_data[0]["day_name"])
@@ -29,4 +34,10 @@ def wind():
 def weather():
     return render_template('weather.html',weather=weather_data[0]["wx_str"])
    
+# Songs I listen to while making this program
+# https://www.youtube.com/watch?v=kO-2zldx61
+# https://www.youtube.com/watch?v=9Bbm-7vdlbs
+# https://www.youtube.com/watch?v=KIa56_PHorE
+# https://www.youtube.com/watch?v=J2LxM1bg034
+# https://www.youtube.com/watch?v=EAj4yGZGUQU
 
